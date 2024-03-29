8e42ef5dbc5c841aca76ff26908d449cdc93e00b
Minjoo <m0522inju@gmail.com>
2024-03-29 20:27:44 +0900

    remove(DBFixtures): 불필요한 Fixtures 삭제

--- src/test/java/chess/db/DBFixtures.java
+++
@@ -1,8 +0,0 @@
-package chess.db;
-
-public class DBFixtures {
-
-    public static ChessGameDBConnector createChessGameDBConnector() {
-        return new ChessGameDBConnector("chess_test");
-    }
-}
